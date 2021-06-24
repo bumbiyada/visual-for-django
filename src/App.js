4b5e39e30153f61de00b185e622d60bb238218f4
Maxim Svistunov <bumbiyada@gmail.com>
2021-06-24 16:01:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import ButtonFunction from './buttons/Butons';
+import axios from 'axios';
 
 function App() {
+
+    const [state, setState] = useState([]);
+
+    useEffect(()=> {
+    axios
+        .get('http://localhost:8000/api/v1/mainapp/list')
+        .then(response => {setState(response.data)})
+        }, [])
+    //console.log(response.data)
+    console.log(state[0])
+    console.log(typeof(state))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Visual</h1>
+      <ButtonFunction></ButtonFunction>
+
+            {state.map((p) => (
+            <ul>
+                <li>{p.id} id</li>
+                <li>{p.Foiv} Foiv</li>
+                <li>{p.Document_type} doc type</li>
+                <li>{String(p.Is_aborted)} is aborted</li>
+                <li>{String(p.Is_done)} is done</li>
+                <li>{String(p.Marked_on_delete)} marked on 'delete'</li>
+            </ul>
+            ))}
+
     </div>
-  );
+
+
+
+    );
 }
 
 export default App;
---
+++ src/buttons/Butons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ButtonFunction(){
+	return (
+		<ul>
+		<li>1</li>
+		<li>2</li>
+		</ul>
+	)
+}
